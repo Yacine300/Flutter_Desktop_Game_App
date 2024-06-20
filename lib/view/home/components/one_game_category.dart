b2312386cb9bd56faf6cd47e55df8a6f5585db37
Yacine300 <herrouelyacine7@gmail.com>
2024-06-20 13:59:23 +0100

    update

--- lib/view/home/components/one_game_category.dart
+++ lib/view/home/components/one_game_category.dart
@@ -17,7 +17,7 @@ class OneGameCategory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Color _color = gamesCategory[index].entries.first.value;
+    Color color = gamesCategory[index].entries.first.value;
     return Stack(
       clipBehavior: Clip.none,
       children: [
@@ -30,7 +30,7 @@ class OneGameCategory extends StatelessWidget {
           padding: const EdgeInsets.symmetric(
               horizontal: PaddingManager.p12, vertical: PaddingManager.p12),
           decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10), color: _color),
+              borderRadius: BorderRadius.circular(10), color: color),
           child: Stack(
             clipBehavior: Clip.none,
             children: [
@@ -39,7 +39,7 @@ class OneGameCategory extends StatelessWidget {
                 bottom: 0,
                 child: CircleAvatar(
                   radius: height / 7,
-                  backgroundColor: _color.withGreen(1).withOpacity(0.08),
+                  backgroundColor: color.withGreen(1).withOpacity(0.08),
                 ),
               ),
               Positioned(
@@ -47,7 +47,7 @@ class OneGameCategory extends StatelessWidget {
                   top: 0,
                   child: CircleAvatar(
                       radius: height / 3,
-                      backgroundColor: _color.withGreen(1).withOpacity(0.06))),
+                      backgroundColor: color.withGreen(1).withOpacity(0.06))),
               Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
                 const Spacer(),
                 Icon(
@@ -56,41 +56,50 @@ class OneGameCategory extends StatelessWidget {
                   size: height * 0.3,
                 ),
                 const Spacer(),
-                Text(
-                  gamesCategory[index]['Category'],
-                  style: currentSelectedCategory == index
-                      ? Theme.of(context).textTheme.titleSmall
-                      : Theme.of(context).textTheme.bodySmall,
-                ),
-                Text(
-                  "12.000 Playing",
-                  style: Theme.of(context).textTheme.bodySmall,
-                )
+                Text(gamesCategory[index]['Category'],
+                    style: currentSelectedCategory == index
+                        ? const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 12)
+                        : const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 12)),
+                const Text("12.000 Playing",
+                    style: TextStyle(
+                        fontSize: 10,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.white))
               ]),
             ],
           ),
         ),
         Positioned(
+          top: 15,
+          right: 15,
           child: Transform.rotate(
             angle: 0.2618,
             child: AnimatedContainer(
-              duration: Duration(milliseconds: 250),
+              duration: const Duration(milliseconds: 250),
               curve: Curves.easeIn,
-              height: 90,
-              width: 90,
+              height: 120,
+              width: 120,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(50),
                 boxShadow: [
                   currentSelectedCategory.value == index
                       ? BoxShadow(
-                          color: Colors.black.withOpacity(0.35), // Shadow color
-                          offset: Offset(0, 2), // Offset of the shadow (x, y)
+                          color: Colors.black.withOpacity(0.4), // Shadow color
+                          offset:
+                              const Offset(0, 2), // Offset of the shadow (x, y)
                           blurRadius: 40.0, // Blur radius
                           spreadRadius: 40.0, // Spread radius
                         )
                       : BoxShadow(
                           color: Colors.black.withOpacity(0.1), // Shadow color
-                          offset: Offset(0, 2), // Offset of the shadow (x, y)
+                          offset:
+                              const Offset(0, 2), // Offset of the shadow (x, y)
                           blurRadius: 20.0, // Blur radius
                           spreadRadius: 2.0, // Spread radius
                         ),
@@ -98,14 +107,14 @@ class OneGameCategory extends StatelessWidget {
               ),
             ),
           ),
-          top: 15,
-          right: 15,
         ),
         Positioned(
+          top: -50,
+          right: -40,
           child: Transform.rotate(
             angle: 0.2618,
             child: AnimatedContainer(
-              duration: Duration(milliseconds: 250),
+              duration: const Duration(milliseconds: 250),
               curve: Curves.easeIn,
               height: height,
               width: width,
@@ -117,15 +126,15 @@ class OneGameCategory extends StatelessWidget {
               ),
             ),
           ),
-          top: -50,
-          right: -40,
         ),
         Positioned(
+          top: 35,
+          right: 0,
           child: AnimatedContainer(
-            duration: Duration(milliseconds: 250),
+            duration: const Duration(milliseconds: 250),
             curve: Curves.easeIn,
-            height: height / 2,
-            width: width / 2,
+            height: height * 0.6,
+            width: width * 0.6,
             decoration: BoxDecoration(
               image: DecorationImage(
                 fit: BoxFit.contain,
@@ -133,8 +142,6 @@ class OneGameCategory extends StatelessWidget {
               ),
             ),
           ),
-          top: 35,
-          right: 0,
         ),
       ],
     );
