07ae4e0f0ec8bf2cf7e798876fa8b8a0519aa1a6
Yacine300 <herrouelyacine7@gmail.com>
2024-06-22 22:18:03 +0100

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:game_app/view/my_app.dart';
 
-import 'package:game_app/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
